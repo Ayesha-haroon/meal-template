92a2bbd85713973ec618fa2460a97941b6e0a8e5
Ayesha-haroon <aye028681@gmail.com>
2024-08-02 17:21:41 +0500

    meal-template
    not-sure-section

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,5 +30,22 @@ $('#banner-slider').owlCarousel({
             items:1
         }
     }
+});
+$('#meal-of-the-day-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
 })
+
 
